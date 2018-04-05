b9f47bb6ebac220360fb5a7159f464b83f6c34f4
Junior Corman <junior.corman@gmail.com>
2018-04-05 13:18:25 -0500

    Lectura de los datos

--- digit-reconizer.py
+++ digit-reconizer.py
@@ -6,3 +6,8 @@ import random as rnd
 # Visualizacion
 import seaborn as sns
 import matplotlib.pyplot as plt
+
+# Lectura de los datos
+train_df = pd.read_csv('~/.kaggle/competitions/digit-recognizer/train.csv')
+test_df = pd.read_csv('~/.kaggle/competitions/digit-recognizer/test.csv')
+combine = [train_df, test_df]
