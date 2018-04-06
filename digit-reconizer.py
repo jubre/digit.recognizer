e7dc8bee9077511ed3a6b69ca2faac9551d8cd36
Junior Corman <junior.corman@gmail.com>
2018-04-06 11:49:35 -0500

    Upate train information

--- digit-reconizer.py
+++ digit-reconizer.py
@@ -8,6 +8,19 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Lectura de los datos
-train_df = pd.read_csv('~/.kaggle/competitions/digit-recognizer/train.csv')
-test_df = pd.read_csv('~/.kaggle/competitions/digit-recognizer/test.csv')
-combine = [train_df, test_df]
+train = pd.read_csv('~/.kaggle/competitions/digit-recognizer/train.csv')
+test = pd.read_csv('~/.kaggle/competitions/digit-recognizer/test.csv')
+combine = [train, test]
+
+# Describiendo la data
+print(train.columns.values)
+
+# Previsualizacion de la data de entrenamiento
+print(train.head())
+print(train.tail())
+
+# Obteniendo la columna de 'label' de la data de entrenamiento
+Y_train = train["label"]
+
+# Eliminando la columna de 'label' de la data de entrenamiento
+X_train = train.drop(labels = ["label"],axis = 1)
