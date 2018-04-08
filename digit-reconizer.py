23e2ace59640f98418e22decd5a0a7c37bc6f733
Junior Corman <junior.corman@gmail.com>
2018-04-07 23:04:50 -0500

    Set a learning rate annealer

--- digit-reconizer.py
+++ digit-reconizer.py
@@ -101,3 +101,10 @@ optimizer = RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
 
 # Compile the model
 model.compile(optimizer = optimizer , loss = "categorical_crossentropy", metrics=["accuracy"])
+
+# Set a learning rate annealer
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc',
+                                            patience=3,
+                                            verbose=1,
+                                            factor=0.5,
+                                            min_lr=0.00001)
