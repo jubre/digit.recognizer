54e9d83d23e245feb3a7d5e9976aa81f5f804229
Junior Corman <junior.corman@gmail.com>
2018-04-07 23:09:11 -0500

    With data augmentation

--- digit-reconizer.py
+++ digit-reconizer.py
@@ -112,3 +112,21 @@ learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc',
 # Turn epochs to 30 to get 0.9967 accuracy
 epochs = 1
 batch_size = 86
+
+# With data augmentation to prevent overfitting (accuracy 0.99286)
+
+datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range = 0.1, # Randomly zoom image
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=False,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+
+
+datagen.fit(X_train)
